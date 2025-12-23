25eba9c518b1823a752ad6da1047147aa4cc6bf9
 Екатерина <fickle@MacBook-Air-Ekaterina.local>
2025-12-23 15:31:28 +0400

    fix

--- main/views.py
+++ main/views.py
@@ -13,7 +13,7 @@ class CustomerViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         fio = request.data.get("fio")
         if not isinstance(fio, str):  # валидация
-            return Response(data={"message": "fio должно быть строкой"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"message": "fio должно быть строкой."}, status=status.HTTP_400_BAD_REQUEST)
         if not fio:
             return Response(data={"message": "fio не может быть пустым"}, status=status.HTTP_400_BAD_REQUEST)
 
